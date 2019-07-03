a81067d0eecb23207fb6e429a87aa04f23b3ae23
Souhair bouallala <souhairbouallala@gmail.com>
2019-07-03 16:00:51 +0200

    store before merge greg's work

--- models/university.js
+++ models/university.js
@@ -4,9 +4,10 @@ const Schema = mongoose.Schema;
 const universitySchema = new Schema({
   name: String,
   description: String,
-  location: { type: { type: String }, coordinates: [Number] }
+  location: [Number]
 });
-universitySchema.index({ location: "2dsphere" });
+
+// universitySchema.index({ location: "2dsphere" });
 
 const universityModel = mongoose.model("University", universitySchema);
 module.exports = universityModel;
