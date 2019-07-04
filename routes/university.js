3b44f6fec0c20f5f4f486ce5d1bccadc6cc284aa
Grégoire Lescuyer <gregoirelescuyer@gmail.com>
2019-07-04 11:50:08 +0200

    map+librarydeleted

--- routes/university.js
+++ routes/university.js
@@ -3,26 +3,38 @@ const router = express.Router();
 const university = require("../models/university");
 
 // POST => to create new university and save it to the DB
-router.post("/", (req, res, next) => {
-  // add the location object
-  let location = {
-    type: "Point",
-    coordinates: [req.body.longitude, req.body.latitude]
-  };
-
-  const newUniversity = new university({
-    name: req.body.name,
-    description: req.body.description,
-    location: location // <= add the location when creating a new university
-  });
-
-  newUniversity.save(error => {
-    if (error) {
-      next(error);
-    } else {
+router.post("/adduniversity", (req, res, next) => {
+  const { country, name, location } = req.body;
+  university
+    .create({
+      country,
+      name,
+      location
+    })
+    .then(resDB => {
       res.redirect("/");
-    }
-  });
+    })
+    .catch(err => {
+      res.redirect("/");
+    });
+
+  // add the location object
+  // let location = {
+  //   type: "Point",
+  //   coordinates: [req.body.longitude, req.body.latitude]
+  // };
+  // const newUniversity = new university({
+  //   name: req.body.name,
+  //   description: req.body.description,
+  //   location: location // <= add the location when creating a new university
+  // });
+  // newUniversity.save(error => {
+  //   if (error) {
+  //     next(error);
+  //   } else {
+  //     res.redirect("/");
+  //   }
+  // });
 });
 
 router.get("/universities/api", (req, res) => {
